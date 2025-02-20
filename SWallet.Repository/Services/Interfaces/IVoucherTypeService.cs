3e5377e2a304881f1b78217b3bb6fcd081ca29e9
banana-w <phuonghiepthuan56@gmail.com>
2025-02-20 13:43:14 +0700

    VoucherTypeService

---
+++ SWallet.Repository/Services/Interfaces/IVoucherTypeService.cs
@@ -0,0 +1,15 @@
+﻿using SWallet.Domain.Paginate;
+using SWallet.Repository.Payload.Request.Voucher;
+using SWallet.Repository.Payload.Response.Voucher;
+
+namespace SWallet.Repository.Services.Interfaces
+{
+    public interface IVoucherTypeService
+    {
+        Task<bool> CreateVoucherType(VoucherTypeRequest request);
+        Task<bool> UpdateVoucherType(VoucherTypeRequest request);
+        Task<bool> DeleteVoucherType(string id);
+        Task<VoucherTypeResponse> GetVoucherTypeById(string id);
+        Task<IPaginate<VoucherTypeResponse>> GetVoucherTypes(string search, int page, int size);
+    }
+}
