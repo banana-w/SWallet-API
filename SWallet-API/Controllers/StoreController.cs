b80cc530c1544e8cf6151f2d6867de920212ff32
SE173396_LeNhatHuy <huylnse173396@fpt.edu.vn>
2025-02-27 19:38:19 +0700

    a

---
+++ SWallet-API/Controllers/StoreController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SWallet.Domain.Paginate;
+using SWallet.Repository.Payload.Request.Store;
+using SWallet.Repository.Payload.Response.Store;
+using SWallet.Repository.Services.Interfaces;
+
+namespace SWallet_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private readonly IStoreService _storeService;
+        private readonly ILogger<StoreController> _logger;
+
+        public StoreController(IStoreService storeService, ILogger<StoreController> logger)
+        {
+            _storeService = storeService;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<StoreResponse>> CreateStore(string accountId, CreateStoreModel creation)
+        {
+            try
+            {
+                var storeResponse = await _storeService.CreateStore(accountId, creation);
+                return Ok(storeResponse);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error creating store");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error creating store");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStoreById(string id)
+        {
+            var storeResponse = await _storeService.GetStoreById(id);
+            if (storeResponse == null)
+            {
+                return NotFound();
+            }
+            return Ok(storeResponse);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IPaginate<StoreResponse>>> GetAllStores(string searchName = "", int page = 1, int size = 10)
+        {
+            try
+            {
+                var result = await _storeService.GetStores(searchName, page, size);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error getting stores");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error getting stores");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateStore(string id,UpdateStoreModel store)
+        {
+            var result = await _storeService.UpdateStore(id, store);
+            return Ok(result);
+        }
+
+       
+    }
+}
---
+++ SWallet.Repository/Payload/Response/Store/StoreResponse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWallet.Repository.Payload.Response.Store
+{
+    public class StoreResponse
+    {
+        public string Id { get; set; }
+        public string BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string BrandLogo { get; set; }
+        public string AreaId { get; set; }
+        public string AreaName { get; set; }
+        public string AreaImage { get; set; }
+        public string AccountId { get; set; }
+        public string StoreName { get; set; }
+        public string UserName { get; set; }
+        public string Avatar { get; set; }
+        public string AvatarFileName { get; set; }
+        public string File { get; set; }
+        public string FileName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public TimeOnly? OpeningHours { get; set; }
+        public TimeOnly? ClosingHours { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        public string Description { get; set; }
+        public bool? State { get; set; }
+        public bool? Status { get; set; }
+        public int? NumberOfCampaigns { get; set; }
+        public int? NumberOfVouchers { get; set; }
+        public int? NumberOfBonuses { get; set; }
+        public decimal? AmountOfBonuses { get; set; }
+    }
+}
