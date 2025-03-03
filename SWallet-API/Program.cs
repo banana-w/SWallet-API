55d3471b96364f018ea024b1ac74fcdd308a8314
banana-w <phuonghiepthuan56@gmail.com>
2025-03-03 15:46:59 +0700

    fix swagger

--- SWallet-API/Program.cs
+++ SWallet-API/Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction() || app.Environment.IsStaging())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
