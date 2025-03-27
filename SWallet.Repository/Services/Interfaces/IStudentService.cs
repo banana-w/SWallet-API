948b0b7b5a3f4a2aeea07dafcd56eab2c48a3434
SE173396_LeNhatHuy <huylnse173396@fpt.edu.vn>
2025-03-27 14:31:59 +0700

    updateStudent

--- SWallet.Repository/Services/Interfaces/IStudentService.cs
+++ SWallet.Repository/Services/Interfaces/IStudentService.cs
@@ -12,7 +12,7 @@ namespace SWallet.Repository.Services.Interfaces
     public interface IStudentService
     {
         Task<StudentResponse> CreateStudentAsync(string accountId, StudentRequest studentRequest);
-        Task<bool> UpdateStudentAsync(string accountId, StudentRequest studentRequest);
+        Task<StudentResponse> UpdateStudentAsync(string accountId, StudentRequest studentRequest);
         Task<IPaginate<StudentResponse>> GetStudentsAsync(string search, bool? isAsc, int pageIndex, int pageSize);
         Task<StudentResponse> GetStudentAsync(string studentId);
         Task<StudentResponse> GetStudentByAccountIdAsync(string accountId);
